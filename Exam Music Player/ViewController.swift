161ea11cb686777030bbf981206fe14e4857fd4c
Ole <olehansson@gmail.com>
2019-10-29 10:47:53 +0100

    Initial Commit

---
+++ Exam Music Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exam Music Player
+//
+//  Created by Ole Hansson on 29/10/2019.
+//  Copyright © 2019 Ole Hansson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
